b976eaecc95a3a4196784e6f00c95ff92e8c8f1c
XMakaNX <makan.j4040@gmail.com>
2016-02-04 19:34:50 +0330

    Update link_lock.lua

--- plugins/link_lock.lua
+++ plugins/link_lock.lua
@@ -1,13 +1,29 @@
-local function run(msg)
-    if not is_momod(msg) and msg.type = 'chat' then
-        --chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return 'No links here!'
+local function run(msg, matches)
+if msg.to.type == 'chat' then
+    if is_owner(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_link'] then
+                lock_ads = data[tostring(msg.to.id)]['settings']['lock_link']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_ads == "yes" then
+        send_large_msg(chat, 'Ads is not allowed here!!')
+        chat_del_user(chat, user, ok_cb, true)
     end
 end
-
-return {patterns = {
-    ".com",
-    "http://",
-    "https://",
-    "adf.ly"
-    }, run = run}
+ end
+return {
+  patterns = {
+    "telegram.me/joinchat/",
+        "telegram.me",
+    "[Ht]ttps://"
+  },
+  run = run
+}
