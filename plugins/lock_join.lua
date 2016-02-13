5c7f2f735db8daf5b28d69e2c0c87dbaad608ed2
cyclonetm <makan.j4040@gmail.com>
2016-02-13 17:19:41 +0330

    Update lock_join.lua

--- plugins/lock_join.lua
+++ plugins/lock_join.lua
@@ -1,18 +1,21 @@
-local function run (msg, matches)
-local data = load_data(_config.moderation.data)
-   if matches[1] == 'chat_add_user_link' then
-        local user_id = msg.from.id
-        if data[tostring(msg.to.id)] then
-        if data[tostring(msg.to.id)]['settings'] then
-        if data[tostring(msg.to.id)]['settings']['lock_join'] == 'yes' then
-      kick_user(user_id, msg.to.id)
-    end
+local function run(msg)
+    if msg.action and msg.action.type then
+    local action = msg.action.type
+    if action == 'chat_add_user_link' then
+      local user_id = msg.from.id
+    if data[tostring(msg.to.id)]['settings']['lock_join'] == 'yes' then
+  if not is_momod(msg) then
+  chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+   -- local msg = 'Join link is Locked !'
+  -- local receiver = msg.to.id
+   -- send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
+            end
+        end
+      end
    end
-end   
- end
- end
-return {
-  patterns = {
-    "^!!tgservice (chat_add_user_link)$"
-  },
-  run = run
+end
+return {patterns = {
+
+  "^!!tgservice (.+)$",
+  
+}, run = run}
